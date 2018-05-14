f4480328b4ba7fe46f8bce2387ff9a9b0f60b4fd
Micha0929 <michal.k0929@wp.pl>
2018-05-14 18:36:43 +0200

    no message

---
+++ CMakeFiles/ZAD2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZAD2.dir/main.c.obj"
+  "ZAD2.pdb"
+  "ZAD2.exe"
+  "ZAD2.exe.manifest"
+  "libZAD2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ZAD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
